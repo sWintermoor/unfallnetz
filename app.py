38651755db71dd1d94a335b5fcfd07c8da83ed2d
markol0212 <meinnameistklausdieter@gmail.com>
2025-04-26 18:01:55 +0200

    Backend eventmarker flask

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 # Erstelle eine Flask-Anwendung
 app = Flask(__name__)
+
+# Create a SocketIO instance
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 # Definiere eine Route (eine URL, die eine Funktion aufruft)
 @app.route('/')
 def home():
     return render_template("index.html")
 
+def new_entry():
+    # Falls der Agent neue Einträge in die Datenbank einfügt, wird diese Funktion ausgeführt.
+    return
+
+def send_event(eventType, eventDate, eventLocation, eventLat, eventLng, eventDescription):
+    socketio.emit('EventCreated', {
+        'type': eventType,
+        'date': eventDate,
+        'location': eventLocation,
+        'lat': eventLat,
+        'lng': eventLng,
+        'description': eventDescription
+})
+
 # Starte die Anwendung
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
