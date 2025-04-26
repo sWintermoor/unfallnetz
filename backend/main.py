cb6172ba7c57aa95fc100a985ff2bda0020063b6
Kamboj, Jai <jai.kamboj@studium.uni-hamburg.de>
2025-04-26 19:09:57 +0200

    db connection with mongodb

--- backend/main.py
+++ backend/main.py
@@ -4,22 +4,6 @@ from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 
 
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-
-uri = "mongodb+srv://jaikamboj:0Ju6y7Vadk1I7NQj@cluster0.cmmgnde.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
-
 
 def main():
     # URL der API
