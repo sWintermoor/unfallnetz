2fcf9fa2f6019072181d7db4f052f7c632993214
sWintermoor <sWintermoor@gmail.com>
2025-12-04 21:08:48 +0100

    fixed updater

--- src/updater.py
+++ src/updater.py
@@ -7,44 +7,44 @@ from .websocket import send_data
 from .utils import utils_parse_date, utils_sort_by_event_date
 
 async def update_system(url, collection, socketio):
-    await asyncio.sleep(10)
+    await asyncio.sleep(2)
 
     while True:
-        print("loop")
 
-        # Check for new entries in source website
-        # TODO: Überprüfen, ob features eine Liste ist
-        response = requests.get(url)
-        data = response.json()
-        features = data.get("features", {})
-        get_last_entry = features[-1]
+        try:
+            # Check for new entries in source website
+            # TODO: Überprüfen, ob features eine Liste ist
+            response = requests.get(url)
+            data = response.json()
+            features = data.get("features", {})
+            sorted_features = utils_sort_by_event_date(features)
+            api_newest_entry_date = sorted_features[0].get("properties", {}).get("start", "Unknown")
 
-        if get_last_entry != newest_entry:
+            db_newest_entry = list(collection.find())[0]
+            db_newest_entry_date = db_newest_entry.get("properties", {}).get("start", "Unknown")
 
-            features = utils_sort_by_event_date(features)
-            if newest_entry:
-                newest_entry_date = utils_parse_date(newest_entry.get("properties", {}).get("start", "Unknown"))
+            if api_newest_entry_date != db_newest_entry_date: 
+                new_features = []
+
+                for entry in sorted_features:
+                    entry_date = utils_parse_date(entry.get("properties", {}).get("start", "Unknown"))                
+                    if entry_date > db_newest_entry_date:
+                        new_features.insert(entry)
+                    else:
+                        # Adding new features to the database
+                        if len(new_features) > 0:
+                            collection.insert_many(new_features)
+                            print(f"Erfolgreich {len(new_features)} Einträge gespeichert.")
+
+                            # Sending new features to the frontend
+                            print("New Features sending...")
+                            send_data(socketio, new_features)
+                            db_newest_entry = new_features[-1]
+                        break
+
             else:
-                newest_entry_date = datetime.min
-            new_features = []
-
-            for entry in features:
-                entry_date = utils_parse_date(entry.get("properties", {}).get("start", "Unknown"))                
-                if entry_date > newest_entry_date:
-                    new_features.append(entry)
-                else:
-                    # Adding new features to the database
-                    if len(new_features) > 0:
-                        collection.insert_many(new_features)
-                        print(f"Erfolgreich {len(new_features)} Einträge gespeichert.")
-
-                        # Sending new features to the frontend
-                        print("New Features sending...")
-                        send_data(socketio, new_features)
-                    break
-            newest_entry = get_last_entry
-
-        else:
-            print("No new data available.")
+                print("No new data available.")
+        except Exception as e:
+            print(f"Ein Fehler ist aufgetreten im Updater: {e}")
 
         await asyncio.sleep(20)
